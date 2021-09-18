c7782cb43fb506801ed25414c84eea01148f5087
toufiqnuur <taufik2017yk@gmail.com>
2021-09-18 20:00:17 +0700

    add: font Inter

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,6 +4,14 @@
   import Footer from "$lib/partials/Footer.svelte";
 </script>
 
+<svelte:head>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
+    rel="stylesheet" />
+</svelte:head>
+
 <Navbar />
 <main class="mt-16">
   <slot />
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,13 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   mode: "jit",
   purge: ["./src/**/*.svelte"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
 };
