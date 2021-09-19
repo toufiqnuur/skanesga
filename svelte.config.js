7926714e790832d657b723258670dc57f2f55542
toufiqnuur <taufik2017yk@gmail.com>
2021-09-19 13:04:21 +0700

    fix: change adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import { mdsvex } from "mdsvex";
 import mdsvexConfig from "./mdsvex.config.js";
 import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-vercel";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,16 +13,11 @@ const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: "#svelte",
-    adapter: adapter({
-      pages: "build",
-      assets: "build",
-      fallback: null,
-    }),
+    adapter: adapter(),
     prerender: {
       enabled: true,
       crawl: true,
     },
-    ssr: false,
   },
 };
 
